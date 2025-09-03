bd28a4c10f7c81256d24c986b66a0a5ef4707504
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-09-03 11:01:50 +0530

    createNewUser function added

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,4 +1,7 @@
 import userModel from "../Model/userModel.js";
+import { validationSchema } from "../Validation/zodScheme.js";
+import { ZodError } from "zod";
+import { hashing } from "../Validation/passwordHashing.js";
 
 export const showALLUsers = async(req,res) => {
     try{
@@ -22,4 +25,54 @@ export const showALLUsers = async(req,res) => {
         });
     }
     
+}
+
+export const createNewUser = async(req,res) => {
+    try{
+        const validFormat = await validationSchema.parseAsync(req.body);
+
+        const existingUser = await userModel.findOne({
+            email: validFormat.email
+        });
+
+        if(existingUser) {
+           return res.status(400).json({
+                success: false,
+                message: "User Already Exists",
+                user: existingUser
+            })
+        }
+        const safePassword = await hashing(validFormat.password);
+
+        const newUser = await userModel.create({
+            ...validFormat,
+            password: safePassword
+        });
+
+        if(!newUser) {
+           return res.status(500).json({
+                success: false,
+                message: "No new users created"
+            })
+        } else {
+           return res.status(201).json({
+                success: true,
+                user: newUser
+            })
+        }
+    }
+    catch(error) {
+        if (error instanceof ZodError) {
+            res.status(400).json({
+                success: false,
+                error: error.issues,
+                message: "Error Request"
+            })
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Invalid Server Request"
+            })
+        }
+    }
 }
--- Route/userRoute.js
+++ Route/userRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { showALLUsers } from '../Controller/userController.js';
+import { showALLUsers,createNewUser } from '../Controller/userController.js';
 
 const router = express.Router();
 
 router.get('/users',showALLUsers);
-
+router.post('/add',createNewUser);
 
 export default router;
