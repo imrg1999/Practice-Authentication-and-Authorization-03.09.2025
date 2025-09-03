7e0f97b7d4380c0278517645db88a1dda244529e
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-09-03 08:10:17 +0530

    User Model created

---
+++ Model/userModel.js
@@ -0,0 +1,32 @@
+import {Schema, model} from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+    
+});
+
+const userModel = model("practise",userSchema);
+
+export default userModel;
